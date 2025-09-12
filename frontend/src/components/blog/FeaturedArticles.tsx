299e3889035e755eb9938347f7a559b2f7f54489
Andre27031510 <andre27031510@gmail.com>
2025-09-12 12:45:33 -0300

    mudanças

--- frontend/src/components/blog/FeaturedArticles.tsx
+++ frontend/src/components/blog/FeaturedArticles.tsx
@@ -135,7 +135,6 @@ export default function FeaturedArticles() {
               </div>
             ))}
           </div>
-        )}
         ) : (
           <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
             {featuredArticles.map((article, index) => (
