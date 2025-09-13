53aadd69af3443c4f236922e2a7e69dbcc426cac
Andre27031510 <andre27031510@gmail.com>
2025-09-13 13:47:03 -0300

    mudanças

--- frontend/src/components/ui/AnimatedElement.tsx
+++ frontend/src/components/ui/AnimatedElement.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { animations, createRippleEffect, animateNumber } from '../../utils/animations'
 
 interface AnimatedElementProps {
